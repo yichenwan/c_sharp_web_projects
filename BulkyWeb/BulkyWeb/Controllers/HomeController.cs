dce5f7c1f4cfe3ac2cf7a74f10b4d31b0ac42dee
Yi-Chen Wan <carrie@yunyun.cloud>
2024-12-30 17:43:10 +0800

    try sql connection operation in privacy view

--- BulkyWeb/BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/BulkyWeb/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BulkyWeb.Models;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace BulkyWeb.Controllers;
 
@@ -20,13 +22,36 @@ public class HomeController : Controller
 
     public IActionResult Privacy()
     {
-        return View();
+        string connectionString = GetConnectionString();
+        SqlConnection conn = new SqlConnection();
+        conn.ConnectionString = connectionString;
+        string sql = "SELECT * From Categories;";
+        SqlDataAdapter adp = new SqlDataAdapter(sql, conn);
+        DataSet ds = new DataSet();
+        adp.Fill(ds);
+        DataTable dt = ds.Tables[0];
+        string str = "";
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            str += $"Name: {dt.Rows[i]["Name"]}<br/>";
+            str += $"DisplayOrder: {dt.Rows[i]["DisplayOrder"]}<hr>";
+        }
+        Console.Write(str);
+        return Content(str);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    static private string GetConnectionString()
+    {
+        // To avoid storing the connection string in your code,
+        // you can retrieve it from a configuration file, using the
+        // System.Configuration.ConfigurationManager.ConnectionStrings property
+        return "server=localhost,1401;user=sa;password=LikeAndSubscribe1!;database=Bulky;TrustServerCertificate=True;Encrypt=False";
     }
 }
 
